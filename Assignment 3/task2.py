94a30e4c162758c35351bbb37e85ca1a0e98232c
arjun <arjunv2001@gmail.com>
2021-11-01 19:42:37 +0530

    fixed A3T2

--- Assignment 3/task2.py
+++ Assignment 3/task2.py
@@ -14,10 +14,13 @@ global_path = sys.argv[2]
 df_city = sql_context.read.csv(city_path, header=True)
 df_global  = sql_context.read.csv(global_path, header=True)
 
+df_global = df_global.withColumn('LandAverageTemperature',df_global['LandAverageTemperature'].cast(FloatType()))
+df_city = df_city.withColumn('AverageTemperature',df_city['AverageTemperature'].cast(FloatType()))
+
 new_df = df_city.join(df_global,df_city.dt == df_global.dt,"inner").drop(df_city.dt)
 
-new_df.AverageTemperature = new_df.AverageTemperature.cast(FloatType())
-new_df.LandAverageTemperature = new_df.LandAverageTemperature.cast(FloatType())
+#new_df.AverageTemperature = new_df.AverageTemperature.cast(FloatType())
+#new_df.LandAverageTemperature = new_df.LandAverageTemperature.cast(FloatType())
 
 new_df = new_df.filter(F.col("AverageTemperature") > F.col("LandAverageTemperature"))
 new_df = new_df.groupBy(new_df.dt, new_df.Country).agg({"AverageTemperature": "max"})
