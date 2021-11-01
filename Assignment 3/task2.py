524c0c688718d9b800e694bc7381b250d5d56da6
arjun <arjunv2001@gmail.com>
2021-11-01 20:47:01 +0530

    fixed A3T2

--- Assignment 3/task2.py
+++ Assignment 3/task2.py
@@ -11,16 +11,19 @@ sql_context=SQLContext(sc)
 city_path = sys.argv[1]
 global_path = sys.argv[2]
 
+
 df_city = sql_context.read.csv(city_path, header=True)
 df_global  = sql_context.read.csv(global_path, header=True)
 
-df_global = df_global.withColumn('LandAverageTemperature',df_global['LandAverageTemperature'].cast(FloatType()))
-df_city = df_city.withColumn('AverageTemperature',df_city['AverageTemperature'].cast(FloatType()))
+df_global=df_global.withColumn('LandAverageTemperature',df_global['LandAverageTemperature'].cast(FloatType()))
+df_city=df_city.withColumn('AverageTemperature',df_city['AverageTemperature'].cast(FloatType()))
 
 new_df = df_city.join(df_global,df_city.dt == df_global.dt,"inner").drop(df_city.dt)
 
-#new_df.AverageTemperature = new_df.AverageTemperature.cast(FloatType())
-#new_df.LandAverageTemperature = new_df.LandAverageTemperature.cast(FloatType())
+new_df.AverageTemperature = new_df.AverageTemperature.cast('float')
+new_df.LandAverageTemperature = new_df.LandAverageTemperature.cast('float')
+
+#print(new_df.dtypes)
 
 new_df = new_df.filter(F.col("AverageTemperature") > F.col("LandAverageTemperature"))
 new_df = new_df.groupBy(new_df.dt, new_df.Country).agg({"AverageTemperature": "max"})
@@ -35,3 +38,5 @@ res = dict(sorted(res.items()))
 
 for key in res:
 	print(key,'\t',res[key])
+
+	
